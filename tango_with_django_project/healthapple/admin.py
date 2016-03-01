74c87525dab8ab7885c57380d9d3c49167be3a40
ocherwei <ocherwei@gmail.com>
2016-03-01 22:45:33 +0000

    tried to display name and surname of user (fail)

--- tango_with_django_project/healthapple/admin.py
+++ tango_with_django_project/healthapple/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from healthapple.models import Category, Page, UserProfile
 
-# Register your models here.
+class PageAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category', 'url', 'readibility_score', 'subjectivity_score', 'sentimentality_score',)
+
+# Add in this class to customized the Admin Interface
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('name', 'surname', 'email',)
+	
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Page, PageAdmin)
+admin.site.register(UserProfile)
