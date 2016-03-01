366d661ba2a13239d1c16720827e43c8393817d7
ocherwei <ocherwei@gmail.com>
2016-03-01 21:28:30 +0000

    added forms.py into healthapple app

---
+++ tango_with_django_project/healthapple/forms.py
@@ -0,0 +1,45 @@
+from django import forms
+from healthapple.models import Page, Category, UserProfile
+from django.contrib.auth.models import UserProfile
+
+class CategoryForm(forms.ModelForm):
+	name = models.CharField(max_length=128)
+	
+	class Meta:
+		model = Category
+		fields = ('name',)
+		
+class PageForm(forms.ModelForm):
+	category = models.ForeignKey(Category)
+	title = models.CharField(max_length=128, help_text="Please enter the title of the page.")
+	url = models.URLField(max_length=200, help_text="Please enter the URL of the page.")
+	readibility_score = models.IntegerField(default = 0)
+	subjectivity_score = models.IntegerField(default = 0)
+	sentimentality_score = models.IntegerField(default = 0)
+	
+	class Meta:
+		model = Page
+		exclude = ('category',)
+		
+	def clean(self):
+        cleaned_data = self.cleaned_data
+        url = cleaned_data.get('url')
+
+        # If url is not empty and doesn't start with 'http://', prepend 'http://'.
+        if url and not (url.startswith('http://') or url.startswith('https://')):
+            url = 'http://' + url
+            cleaned_data['url'] = url
+
+        return cleaned_data
+
+class UserForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput())
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password')
+
+class UserProfileForm(forms.ModelForm):
+    class Meta:
+        model = UserProfile
+        fields = ('picture')
