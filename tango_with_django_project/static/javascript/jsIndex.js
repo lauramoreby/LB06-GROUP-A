ffb080a9e1295ed2c91a006fef150c97eb103329
Baetek <bjuszczak1@gmail.com>
2016-03-18 19:41:13 +0000

    Added toast notification system

--- tango_with_django_project/static/javascript/jsIndex.js
+++ tango_with_django_project/static/javascript/jsIndex.js
@@ -1,48 +1,67 @@
-
-function init() { // Runs when the page is first loaded
+if (localStorage.getItem("passChange") == null) {
+  localStorage.setItem("passChange", 'false');
+}
+if (localStorage.getItem("login") == null) {
+  localStorage.setItem("login", 'false');
+}
+if (localStorage.getItem("logout") == null) {
+  localStorage.setItem("logout", 'false');
 }
 
-$(document).ready( function () {
-  alert("hi");
+function notificationDisplay() {
+  if (localStorage.getItem("passChange") == 'true') {
+    Materialize.toast('Password changed succesfully!', 4000, '');
+    localStorage.setItem("passChange", 'false');
+  }
+  if (localStorage.getItem("login") == 'true') {
+    Materialize.toast('You are now logged in!', 4000, 'left');
+    localStorage.setItem("login", 'false');
+  }
+  if (localStorage.getItem("logout") == 'true') {
+    Materialize.toast('You are now logged out!', 4000, '');
+    localStorage.setItem("logout", 'false');
+  }
+}
+
+function notificationEnabler(type) {
+  localStorage.setItem(type, 'true');
 }
 
 function createCard(content) {
-    return "<div class = 'main_content_card result_card'><div class = 'inner_main_content_card'>" + content + "</div></div><br>"
+  return "<div class = 'main_content_card result_card'><div class = 'inner_main_content_card'>" + content + "</div></div><br>";
 }
 
 function toTop() { // Scrolls to top of page
-    $('html, body').animate({ scrollTop: 0 }, 'fast');
+  $('html, body').animate({ scrollTop: 0 }, 'fast');
 }
 
 function onScroll() {
-	alert("hi");
-    loseSearchFocus();
-    if ($("body").scrollTop() != 0) {topBarShadow.show();}
-    else {topBarShadow.hide();}
+  loseSearchFocus();
+  if ($("body").scrollTop() != 0) {topBarShadow.show();}
+  else {topBarShadow.hide();}
 }
 
 function loseSearchFocus() {
-    $("#clubSearch").blur();
+  $("#clubSearch").blur();
 }
 
 function getSearchFocus() { // Scrolls to top of page, resets search box value and gives it focus
-    anim = Promise.resolve($('html, body').animate({ scrollTop: 0 }, 'fast').promise());
-    Promise.all([anim]).then(function () {
-        $("#clubSearch").val('');
-        $("#clubSearch").focus();
-    });
+  anim = Promise.resolve($('html, body').animate({ scrollTop: 0 }, 'fast').promise());
+  Promise.all([anim]).then(function () {
+      $("#clubSearch").val('');
+      $("#clubSearch").focus();
+  });
 }
 
 var topBarShadow = {
-    show: function() {
-        $("#top_bar").css("box-shadow","0px -6px 6px 6px #000000");
-    },
-    hide: function() {
-        $("#top_bar").css("box-shadow","");
-    }
-}
+  show: function() {
+      $("#top_bar").css("box-shadow","0px -6px 6px 6px #000000");
+  },
+  hide: function() {
+      $("#top_bar").css("box-shadow","");
+  }
+};
 
 function passChange() {
-	alert("hi");
 	$('.error').fadeIn(400).delay(3000).fadeOut(400); //fade out after 3 seconds
 }
