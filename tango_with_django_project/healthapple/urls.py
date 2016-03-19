90ceef4994e550e46d489fd5d045e05e096454d9
ocherwei <ocherwei@gmail.com>
2016-03-19 13:27:49 +0000

    added name for url mapping for healthapplesearchapi

--- tango_with_django_project/healthapple/urls.py
+++ tango_with_django_project/healthapple/urls.py
@@ -4,6 +4,6 @@ from healthapple import views
 urlpatterns = [
 	
 	url(r'^$',views.index, name='index'),
-	url(r'^healthapplesearchapi/', views.search,),
+	url(r'^healthapplesearchapi/', views.search, name='search_api'),
 	url(r'^user_profile/',views.user_profile, name='user_profile'),
 ]
