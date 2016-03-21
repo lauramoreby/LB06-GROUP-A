3d4fb0943acde7d695847a7e5f01b2e047d62a8a
mohsinullah <mohsinullah95@gmail.com>
2016-03-21 10:27:13 +0000

    wut

--- tango_with_django_project/healthapple/healthfinder_api.py
+++ tango_with_django_project/healthapple/healthfinder_api.py
@@ -41,8 +41,6 @@ def run_query(search_terms):
                   url = result['MoreInfo'][0]['Url']
               else:
                   url = result['MoreInfo']['Url']
-              if result['Url'][-1] == "/":
-                result['Url'] = result['Url'][:-1]
               results.append({
               'title': result['Title'],
               'link': url,
