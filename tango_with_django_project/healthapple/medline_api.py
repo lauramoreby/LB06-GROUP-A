f26d09729287659913d564e9478a75c0f3ff5564
mohsinullah <mohsinullah95@gmail.com>
2016-03-21 10:57:11 +0000

    Fixed everything

--- tango_with_django_project/healthapple/medline_api.py
+++ tango_with_django_project/healthapple/medline_api.py
@@ -1,10 +1,12 @@
 import json
 import urllib, urllib2
 
-import xmltodict as xmltodict
+import xmltodict
 from textstat.textstat import textstat
 from textblob import TextBlob
 import xml.etree.ElementTree
+from xml.dom import minidom
+
 
 
 
@@ -30,37 +32,46 @@ def run_query(search_terms):
         response = urllib2.urlopen(search_url).read()
 
         # Open file and write xml code to file to be parsed later on
-        file = open("temp_xml.txt","w")
-        file.truncate()
-        file.write(response)
-        file.close()
+        # file = open("temp.xml","w")
+        # file.truncate()
+        # file.write(response)
 
 
         # Convert the string response to a Python dictionary object.
-        parse_response = xml.etree.ElementTree.parse("temp_xml.txt").getroot()
+
+        parse_response = xmltodict.parse(response)
+        #print parse_response
+        # parse_response = minidom.parse("temp.xml")
+        # itemlist = parse_response.getElementsByTagName()
+        #return type(parse_response)
+        #return
+
+        #file.close()
 
 
         # Loop through each page returned, populating out results list.
+        l=[]
         for result in parse_response:
-            blob = TextBlob(result['Description'])
-            for sentence in blob.sentences:
-                polarity_score = sentence.sentiment.polarity
-                subjectivity_score = sentence.sentiment.subjectivity
-                
-            results.append({
-            'title': result['Title'],
-            'link': result['Url'],
-            'summary': result['Description'],
-            'flesch_score': '{0:.2f}'.format(textstat.flesch_reading_ease(result['Description'])),
-            'polarity_score': '{0:.2f}'.format(polarity_score),
-            'subjectivity_score': '{0:.2f}'.format(subjectivity_score),
-            'source':'MedlinePlus'})
-
-
-    # Catch a URLError exception - something went wrong when connecting!
+            l+= [parse_response[result]]
+        return l
+    #         blob = TextBlob(result['Description'])
+    #         for sentence in blob.sentences:
+    #             polarity_score = sentence.sentiment.polarity
+    #             subjectivity_score = sentence.sentiment.subjectivity
+    #
+    #         results.append({
+    #         'title': result['Title'],
+    #         'link': result['Url'],
+    #         'summary': result['Description'],
+    #         'flesch_score': '{0:.2f}'.format(textstat.flesch_reading_ease(result['Description'])),
+    #         'polarity_score': '{0:.2f}'.format(polarity_score),
+    #         'subjectivity_score': '{0:.2f}'.format(subjectivity_score),
+    #         'source':'MedlinePlus'})
+    #
+    #
+    # # Catch a URLError exception - something went wrong when connecting!
     except urllib2.URLError as e:
-        print "Error when querying the Bing API: ", e
-
-    # Return the list of results to the calling function.
-    return results
-
+         print "Error when querying the Bing API: ", e
+    #
+    # # Return the list of results to the calling function.
+    # return results
