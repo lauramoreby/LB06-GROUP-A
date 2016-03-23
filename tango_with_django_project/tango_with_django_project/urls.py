cc0daca23caf1e79fe93f5ab8bdd6d2415fd5426
ocherwei <ocherwei@gmail.com>
2016-03-23 12:37:12 +0000

    changed the mapping of url for accounts

--- tango_with_django_project/tango_with_django_project/urls.py
+++ tango_with_django_project/tango_with_django_project/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import patterns, include, url
 from django.conf import settings
 from registration.backends.simple.views import RegistrationView
 from django.contrib import admin
+from healthapple import views
 
 class MyRegistrationView(RegistrationView):
     def get_success_url(self,request):
@@ -14,8 +15,8 @@ urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
 	url(r'^healthapple/',include('healthapple.urls')),
-	url(r'^healthapple/register/$', MyRegistrationView.as_view(), name='registration_register'),
-	url(r'^healthapple/',include('registration.backends.simple.urls')),
+	url(r'^accounts/register/$', MyRegistrationView.as_view(), name='registration_register'),
+	url(r'^accounts/',include('registration.backends.simple.urls')),
 	
 )
 
