d06734fbbf5b3a1fa86e4405c65057ef651c3f7a
ocherwei <ocherwei@gmail.com>
2016-03-23 19:18:14 +0000

    trying to cleanup medline_api.py

--- tango_with_django_project/healthapple/medline_api.py
+++ tango_with_django_project/healthapple/medline_api.py
@@ -1,14 +1,6 @@
 import json
 import urllib, urllib2
 
-<<<<<<< HEAD
-import xmltodict
-from textstat.textstat import textstat
-from textblob import TextBlob
-import xml.etree.ElementTree
-from xml.dom import minidom
-
-=======
 from textstat.textstat import textstat
 from textblob import TextBlob
 import xml.etree.ElementTree
@@ -17,9 +9,6 @@ import xml.etree.ElementTree as ET
 import sys
 from xml.dom import minidom
 from bs4 import BeautifulSoup as Soup
->>>>>>> eff4179f86a6151ff14c3e8e896843d24a4f053c
-
-
 
 if __name__ == '__main__':
     main()
@@ -41,25 +30,6 @@ def run_query(search_terms):
     try:
         # Connect to the server and read the response generated.
         response = urllib2.urlopen(search_url).read()
-
-<<<<<<< HEAD
-        # Open file and write xml code to file to be parsed later on
-        # file = open("temp.xml","w")
-        # file.truncate()
-        # file.write(response)
-
-
-        # Convert the string response to a Python dictionary object.
-
-        parse_response = xmltodict.parse(response)
-        #print parse_response
-        # parse_response = minidom.parse("temp.xml")
-        # itemlist = parse_response.getElementsByTagName()
-        #return type(parse_response)
-        #return
-
-        #file.close()
-=======
 
         # Open file and write xml code to file to be parsed later on
         soup = Soup(response)
@@ -88,10 +58,7 @@ def run_query(search_terms):
                 'polarity_score': '{0:.2f}'.format(polarity_score),
                 'subjectivity_score': '{0:.2f}'.format(subjectivity_score),
                 'source':'MedlinePlus'})
-
-
-
-
+                
         dict = {}
 
         keynum = 0
@@ -124,40 +91,11 @@ def run_query(search_terms):
             # 'polarity_score': '{0:.2f}'.format(polarity_score),
             # 'subjectivity_score': '{0:.2f}'.format(subjectivity_score),
             # 'source':'MedlinePlus'})
->>>>>>> eff4179f86a6151ff14c3e8e896843d24a4f053c
-
-
-        # Loop through each page returned, populating out results list.
-        l=[]
-        for result in parse_response:
-            l+= [parse_response[result]]
-        return l
-    #         blob = TextBlob(result['Description'])
-    #         for sentence in blob.sentences:
-    #             polarity_score = sentence.sentiment.polarity
-    #             subjectivity_score = sentence.sentiment.subjectivity
-    #
-    #         results.append({
-    #         'title': result['Title'],
-    #         'link': result['Url'],
-    #         'summary': result['Description'],
-    #         'flesch_score': '{0:.2f}'.format(textstat.flesch_reading_ease(result['Description'])),
-    #         'polarity_score': '{0:.2f}'.format(polarity_score),
-    #         'subjectivity_score': '{0:.2f}'.format(subjectivity_score),
-    #         'source':'MedlinePlus'})
-    #
-    #
     # # Catch a URLError exception - something went wrong when connecting!
     except urllib2.URLError as e:
-<<<<<<< HEAD
-         print "Error when querying the Bing API: ", e
-    #
-    # # Return the list of results to the calling function.
-    # return results
-=======
+
         print "Error when querying the MedlinePlus API: ", e
 
     # Return the list of results to the calling function.
     return results
 
->>>>>>> eff4179f86a6151ff14c3e8e896843d24a4f053c
